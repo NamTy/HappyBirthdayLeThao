a1e084ed11ccc746f10e22e8d01463a35082d100
Nam Ty <trinhhongnam171020201220@gmail.com>
2022-11-18 18:22:39 +0700

    first commit

---
+++ js/countdown.js
@@ -0,0 +1,13 @@
+timeLeft = 3;
+
+function countdown() {
+  timeLeft--;
+  document.getElementById("seconds").innerHTML = String(timeLeft);
+  if (timeLeft > 0) {
+    setTimeout(countdown, 1000);
+  } else {
+      window.location = "../birthday.html";
+  }
+}
+
+setTimeout(countdown, 1000);
